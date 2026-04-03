571fd52c6083cc25562281ce1c8dd66f34de6168
luckyseungjae-maker <luckyseungjae@naver.com>
2026-04-03 14:53:37 +0900

    Update fix-quotes.js

--- fix-quotes.js
+++ fix-quotes.js
@@ -5,5 +5,6 @@ c = c.split(String.fromCharCode(8220)).join(String.fromCharCode(34));
 c = c.split(String.fromCharCode(8221)).join(String.fromCharCode(34));
 c = c.split(String.fromCharCode(8216)).join(String.fromCharCode(39));
 c = c.split(String.fromCharCode(8217)).join(String.fromCharCode(39));
+c = c.split(String.fromCharCode(8230)).join(`...`);
 fs.writeFileSync(p, c, `utf8`);
 console.log(`Done!`);
