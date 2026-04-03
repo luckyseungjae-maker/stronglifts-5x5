6c57ab79c634647d07ec2addb709f3e0cfa5039d
luckyseungjae-maker <luckyseungjae@naver.com>
2026-04-03 12:41:15 +0900

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDO9ChzGxXiTdd5Pk_FsPVE4HwJMtWqoKk",
+  authDomain: "x5-work-out.firebaseapp.com",
+  projectId: "x5-work-out",
+  storageBucket: "x5-work-out.firebasestorage.app",
+  messagingSenderId: "281822760462",
+  appId: "1:281822760462:web:4517c3b595ef12bfddf65f",
+  measurementId: "G-8G5S977BKK"
+};
+
+export default firebaseConfig;
